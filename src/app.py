1897e87ba5898f43b56dae5cb1a9ce75fd70e0b7
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2023-01-27 23:27:28 -0300

    Add color gradient

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageDraw
 
-from color import get_random_color
+from color import get_random_color, Color, get_color_matrix
 from mesh import Resolution, Size, TetragonMesh
 
 
@@ -8,6 +8,13 @@ def main():
     resolution = Resolution(32, 18)
     size = Size(1920, 1080)
     mesh = TetragonMesh(size, resolution)
+    colors = [
+        Color(255, 0, 0),
+        Color(0, 0, 255),
+        Color(255, 0, 255),
+        Color(0, 0, 0),
+    ]
+    matrix = get_color_matrix(colors, resolution)
 
     image = Image.new("RGB", size)
     drawing = ImageDraw.Draw(image)
@@ -16,9 +23,9 @@ def main():
         for y in range(resolution.y):
             tetragon = mesh.get_tetragon(x, y)
             if tetragon:
-                drawing.polygon(tetragon.as_primitives(), fill=get_random_color())
+                drawing.polygon(tetragon.as_primitives(), fill=matrix[x][y])
 
-    image.show()
+    image.save("test.png")
 
 
 if __name__ == "__main__":
--- src/color.py
+++ src/color.py
@@ -1,7 +1,40 @@
 import random
 
-get_random_color = lambda: (
+from typing import NamedTuple
+
+
+class Color(NamedTuple):
+    r: int
+    g: int
+    b: int
+
+
+get_random_color = lambda: Color(
     random.randint(0, 255),
     random.randint(0, 255),
     random.randint(0, 255),
 )
+
+def get_color_gradient(start: Color, end: Color, n: int):
+    r = int((end.r - start.r) / n)
+    g = int((end.g - start.g) / n)
+    b = int((end.b - start.b) / n)
+
+    return [
+        Color(
+            start.r + r * i,
+            start.g + g * i,
+            start.b + b * i,
+        )
+        for i in range(n)
+    ]
+
+
+def get_color_matrix(colors, resolution):
+    g_first = get_color_gradient(colors[0], colors[1], resolution.x)
+    g_last = get_color_gradient(colors[3], colors[2], resolution.x)
+    cols = []
+    for c in range(resolution.x):
+        g = get_color_gradient(g_first[c], g_last[c], resolution.y)
+        cols.append(g)
+    return cols
